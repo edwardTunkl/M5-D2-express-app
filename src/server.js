c88288c80ee5711ac2b9237197aabfd38d853803
edwardTunkl <eddytunkl@googlemail.com>
2021-09-14 20:11:34 +0200

    methods added

--- src/server.js
+++ src/server.js
@@ -2,11 +2,19 @@ import express from "express"
 import listEndpoints from "express-list-endpoints"
 import cors from "cors"
 
+import authorsRouter from "./services/authors/index.js"
+
 const server = express()
 const port = 3001
 
-server.use(cors())
-server.use(express.json())
+server.use(cors())    // Add this to make your FE be able to communicate with BE
+server.use(express.json())    // If I do not specify this line BEFORE the routes, all the requests' bodies will be UNDEFINED
+
+// --Endpoints--
+
+server.use("/authors", authorsRouter)
+
+console.table(listEndpoints(server))
 
 server.listen(port, () => {
   console.log("SERVER RUNNING ON PORT", port)
---
+++ src/services/authors/index.js
@@ -0,0 +1,89 @@
+import express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import uniqid from "uniqid";
+
+const authorsRouter = express.Router();
+
+const currentFilePath = fileURLToPath(import.meta.url);
+// console.log("IMPORT META URL ", import.meta.url);
+// console.log("CURRENT FILE PATH: ", currentFilePath);
+const currentDirPath = dirname(currentFilePath);
+// console.log("CURRENT DIRECTORY: ", currentDirPath);
+const authorsJSONFilePath = join(currentDirPath, "authors.json");
+// console.log("Authors.json path: ", authorsJSONFilePath);
+
+//---POST---
+
+authorsRouter.post("/", (req, res) => {
+  console.log("REQUESTBODY: ", req.body);
+
+  const newAuthor = { ...req.body, id: uniqid(), createdAt: new Date() };
+  console.log(newAuthor);
+
+  const authors = JSON.parse(fs.readFileSync(authorsJSONFilePath));
+
+  authors.push(newAuthor);
+
+  fs.writeFileSync(authorsJSONFilePath, JSON.stringify(authors));
+
+  res.status(201).send({ id: newAuthor.id });
+});
+
+//---GET---
+
+authorsRouter.get("/", (req, res) => {
+  const fileContent = fs.readFileSync(authorsJSONFilePath);
+  console.log(JSON.parse(fileContent));
+
+  const authors = JSON.parse(fileContent);
+
+  res.send(authors);
+});
+
+
+
+//---GET---
+
+authorsRouter.get("/:id", (req, res) =>{
+
+const authors = JSON.parse(fs.readFileSync(authorsJSONFilePath))
+
+const author = authors.find(a => a.id === req.params.id)
+
+if (author){
+  res.send(author)
+ } else {
+  res.send("Not found!")
+ }
+})
+
+//---PUT---
+
+authorsRouter.put("/:id", (req, res) => {
+
+  const authors = JSON.parse(fs.readFileSync(authorsJSONFilePath))
+
+  const remainingAuthors = authors.filter(auth => auth.id !== req.params.id)
+  
+  const updatedAuthor = {...req.body, id: req.params.id}
+
+  remainingAuthors.push(updatedAuthor)
+
+  fs.writeFileSync(authorsJSONFilePath, JSON.stringify(authors))
+  res.send(updatedAuthor)
+
+})
+
+//---DELETE---
+
+authorsRouter.delete("/:id", (req, res) => {
+
+  const authors = JSON.parse(fs.readFileSync(authorsJSONFilePath))
+  const remainingAuthors = authors.filter(auth => auth.id !== req.params.id)
+
+  fs.writeFileSync(authorsJSONFilePath, JSON.stringify(remainingAuthors))
+  res.status(204).send()
+})
+export default authorsRouter;
